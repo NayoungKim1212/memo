25ae4b7c48da65474dd8c0fdad9f67ec80f85c53
NayoungKim1212 <kkd04250@naver.com>
2023-06-26 11:46:03 +0900

    Spring Data JPA란 무엇일까?

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,7 +4,7 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
-import org.springframework.stereotype.Component;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -34,20 +34,17 @@ public class MemoService { // memoService라는 이름으로 등록이 됨
     public List<MemoResponseDto> getMemos() {
         // DB 조회
 
-        return memoRepository.findAll();
+        return memoRepository.findAll().stream().map(MemoResponseDto::new).toList();
     }
-
+    @Transactional
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
 
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if (memo != null) {
-            // memo 내용 수정
-            memoRepository.update(id, requestDto);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        Memo memo = findMemo(id);
+
+        // memo 내용 수정
+        memo.update(requestDto);
+        return id;
     }
 
 
@@ -56,13 +53,17 @@ public class MemoService { // memoService라는 이름으로 등록이 됨
     public Long deleteMemo(Long id) {
 
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if (memo != null) {
-            // memo 삭제
-            memoRepository.delete(id);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        Memo memo = findMemo(id);
+
+        // memo 삭제
+        memoRepository.delete(memo);
+        return id;
+
+    }
+
+    private Memo findMemo(Long id) {
+        return memoRepository.findById(id).orElseThrow(() -> // memo가 null 값이 아니라면 memo 반환
+                new IllegalArgumentException("선택한 메모는 존재하지 않습니다.") // 데이터베이스에 아무것도 없으면 null이 반환
+        );
     }
 }
